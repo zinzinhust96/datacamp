7cf613831fe441cce23827f3b6eddd7d68dd663a
Damini Jain <39940054+jaindamini1111@users.noreply.github.com>
2019-06-28 09:27:06 +0530

    Update 07-finding-important-collaborators.py
    The code had an error, it was "deg_cent" in place of "dc" which wasn't correct because in this list comprehension "degree centrality of the current node" i.e. "dc" had to be equal to the "maximum degree centrality" i.e. "max_dc" to find the users.

--- 21-network-analysis-in-python-(part-1)/04-bringing-it-all-together/07-finding-important-collaborators.py
+++ 21-network-analysis-in-python-(part-1)/04-bringing-it-all-together/07-finding-important-collaborators.py
@@ -18,7 +18,7 @@ deg_cent = nx.degree_centrality(G)
 max_dc = max(deg_cent.values())
 
 # Find the user(s) that have collaborated the most: prolific_collaborators
-prolific_collaborators = [n for n, dc in deg_cent.items() if deg_cent == max_dc]
+prolific_collaborators = [n for n, dc in deg_cent.items() if dc == max_dc]
 
 # Print the most prolific collaborator(s)
 print(prolific_collaborators)
