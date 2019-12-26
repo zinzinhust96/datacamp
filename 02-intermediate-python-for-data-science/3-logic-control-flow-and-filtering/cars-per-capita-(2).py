5f234a8649df07d419620e593a3f42ff44694530
Beta Uliansyah <beta.uliansyah@gmail.com>
2019-12-26 08:53:20 +0700

    Update cars-per-capita-(2).py
    Ignore previous PR, this one should be right

--- 02-intermediate-python-for-data-science/3-logic-control-flow-and-filtering/cars-per-capita-(2).py
+++ 02-intermediate-python-for-data-science/3-logic-control-flow-and-filtering/cars-per-capita-(2).py
@@ -22,20 +22,6 @@ import numpy as np
 
 # Create medium: observations with cars_per_cap between 100 and 500
 cpc = cars['cars_per_cap']
-between = np.logical_and(cpc > 10, cpc < 80)
-medium = cars[between]
-
-# Print medium
-print(medium)
-# Import cars data
-import pandas as pd
-cars = pd.read_csv('cars.csv', index_col = 0)
-
-# Import numpy, you'll need this
-import numpy as np
-
-# Create medium: observations with cars_per_cap between 100 and 500
-cpc = cars['cars_per_cap']
 between = np.logical_and(cpc > 100, cpc < 500)
 medium = cars[between]
 
